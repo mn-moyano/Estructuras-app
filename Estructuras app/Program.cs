e8314070099d16b7ff088b55a2c80b7108fdb3b7
mn-moyano <mn.moyanoz@uea.edu.ec>
2025-06-08 13:28:17 -0500

    Tarea semana 2 completada

--- Estructuras app/Program.cs
+++ Estructuras app/Program.cs
@@ -1,34 +1,63 @@
-﻿Console.WriteLine("UNIVERSIDAD ESTATAL AMAZÓNICA");
-Console.WriteLine("=============================");
-Console.WriteLine();
-
-System.Console.WriteLine("El perimetro de un cuadrado de lado 5 es " + perimetroCuadrado(5));
-System.Console.WriteLine("El área de un cuadrado de lado 5 es " + areaCuadrado(5));
-
-
-
-//int a = 3;
-//int b = 4;
-//Console.WriteLine(a + b);
-//System.Console.WriteLine(suma(3, 4));
-//System.Console.WriteLine(suma(4, 7));
-//System.Console.WriteLine("La suma es: " + suma(2, 1));
-//System.Console.WriteLine("La suma de " + a + " y " + b + " es: " + suma(a, b));
-
-//Crear una función de suma
-//int suma (int a, int b)
-//{
-//    return a + b;
-//}
-
-
-//Función para calcular el perímetro de un cuadrado o rectángulo
-double perimetroCuadrado(double lado)
+﻿//Crear funciones para calcular el área y el perímetro de dos figuras geométricas (Rectángulo y Círculo)
+//CalcularArea es una función que devuelve un valor double, se utiliza para calcular el área del rectángulo donde se requiere datos como base y altura. También se utiliza para calcular el área del círculo y se requiere el radio
+using System;
+namespace PracticaFiguras
 {
-    return lado + lado + lado + lado;
-}
-//Función para calcular el área de un cuadrado
-double areaCuadrado(double lado)
-{
-    return lado * lado;
+    class Programa
+    {
+        static void Main()
+        {
+            Console.WriteLine("UNIVERSIDAD ESTATAL AMAZÓNICA");
+            Console.WriteLine("=============================");
+            Console.WriteLine();
+            //Cálculo de área y perímetro de rectángulo y círculo
+            Console.WriteLine("TAREA SEMANA 2");
+            Console.WriteLine();
+            //Ejecuta el programa
+            Circulo miCirculo = new Circulo(5);
+            Console.WriteLine("Círculo: ");
+            Console.WriteLine("Área: " + miCirculo.CalcularArea());
+            Console.WriteLine("Perímetro: " + miCirculo.CalcularPerimetro());
+
+            Rectangulo miRectangulo = new Rectangulo(4, 6);
+            Console.WriteLine("\nRectángulo: ");
+            Console.WriteLine("Área: " + miRectangulo.CalcularArea());
+            Console.WriteLine("Perímetro: " + miRectangulo.CalcularPerimetro());
+
+        }
+    }
+    class Circulo //Crear la clase circulo
+    {
+        private double radio; //Colocar datos
+        public Circulo(double r)
+        {
+            radio = r;
+        }
+        public double CalcularArea() //Colocar la función
+        {
+            return Math.PI * radio * radio;
+        }
+        public double CalcularPerimetro()
+        {
+            return 2 * Math.PI * radio;
+        }
+    }
+    class Rectangulo //Crear la clase Rectángulo
+    {
+        private double largo;
+        private double ancho; //Colocar los datos
+        public Rectangulo(double l, double a)
+        {
+            largo = l;
+            ancho = a;
+        }
+        public double CalcularArea() //Utilizar las funciones
+        {
+            return largo * ancho;
+        }
+        public double CalcularPerimetro()
+        {
+            return 2 * (largo + ancho);
+        }
+    } 
 }
