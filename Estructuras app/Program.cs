4f933fc6485f74a7f07eda3135e9a9c9e4ee6782
mn-moyano <mn.moyanoz@uea.edu.ec>
2025-06-14 10:04:31 -0500

    Tarea semana 2

--- Estructuras app/Program.cs
+++ Estructuras app/Program.cs
@@ -19,37 +19,3 @@ Console.WriteLine("\nRectángulo: ");
 Console.WriteLine("Área: " + miRectangulo.CalcularArea());
 Console.WriteLine("Perímetro: " + miRectangulo.CalcularPerimetro());
 
-class Circulo //Crear la clase circulo
-{
-    private double radio; //Colocar datos
-    public Circulo(double r)
-    {
-        radio = r;
-    }
-    public double CalcularArea() //Colocar la función
-    {
-        return Math.PI * radio * radio;
-    }
-    public double CalcularPerimetro()
-    {
-        return 2 * Math.PI * radio;
-    }
-}
-class Rectangulo //Crear la clase Rectángulo
-{
-    private double largo;
-    private double ancho; //Colocar los datos
-    public Rectangulo(double l, double a)
-    {
-        largo = l;
-        ancho = a;
-    }
-    public double CalcularArea() //Utilizar las funciones
-    {
-        return largo * ancho;
-    }
-    public double CalcularPerimetro()
-    {
-        return 2 * (largo + ancho);
-    }
-} 
