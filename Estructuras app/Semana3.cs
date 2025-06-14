25b169580db2653048bf289d574ff6b453f3d886
mn-moyano <mn.moyanoz@uea.edu.ec>
2025-06-14 10:00:53 -0500

    Clase Asignatura - Array

--- Estructuras app/Semana3.cs
+++ Estructuras app/Semana3.cs
@@ -1,4 +1,26 @@
-//Console.WriteLine("Universidad Estatal Amazónica");
-//Console.WriteLine("=============================");
-//System.Console.WriteLine();
+//Tarea Semana 3
+using System.Runtime.CompilerServices;
+
+public class Asignatura
+{
+    private string nombre;
+    private double[] notas;
+    //genero el constructor
+    public Asignatura(string _nombre, double[] _notas)
+    {
+        nombre = _nombre;
+        notas = new double[3];
+        notas = _notas;
+    }
+
+    public string getAsignatura()
+    {
+        return nombre;
+    }
+
+    public double[] getNotas()
+    {
+        return notas;
+    }
+}
 
