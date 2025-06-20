64bcae1a5d527ea2d9e6373a441bd2d082a56110
mn-moyano <mn.moyanoz@uea.edu.ec>
2025-06-19 20:22:34 -0500

    Creación Clase Estudiante

--- Estructuras app/Program.cs
+++ Estructuras app/Program.cs
@@ -4,28 +4,30 @@ Console.WriteLine("UNIVERSIDAD ESTATAL AMAZÓNICA");
 Console.WriteLine("=============================\n");
 System.Console.WriteLine();
 
+
+
 //Cálculo de área y perímetro de rectángulo y círculo
-Console.WriteLine("TAREA SEMANA 2\n");
+// Console.WriteLine("TAREA SEMANA 2\n");
 
 
-//Ejecuta el programa
-Circulo miCirculo = new Circulo(5);
-Console.WriteLine("Círculo: ");
-Console.WriteLine("Área: " + miCirculo.CalcularArea());
-Console.WriteLine("Perímetro: " + miCirculo.CalcularPerimetro());
+// //Ejecuta el programa
+// Circulo miCirculo = new Circulo(5);
+// Console.WriteLine("Círculo: ");
+// Console.WriteLine("Área: " + miCirculo.CalcularArea());
+// Console.WriteLine("Perímetro: " + miCirculo.CalcularPerimetro());
 
-Rectangulo miRectangulo = new Rectangulo(4, 6);
-Console.WriteLine("\nRectángulo: ");
-Console.WriteLine("Área: " + miRectangulo.CalcularArea());
-Console.WriteLine("Perímetro: " + miRectangulo.CalcularPerimetro());
-System.Console.WriteLine();
+// Rectangulo miRectangulo = new Rectangulo(4, 6);
+// Console.WriteLine("\nRectángulo: ");
+// Console.WriteLine("Área: " + miRectangulo.CalcularArea());
+// Console.WriteLine("Perímetro: " + miRectangulo.CalcularPerimetro());
+// System.Console.WriteLine();
 
-//Ejecuta semana3
-Estudiante estudiante = new Estudiante("a1", "María Elena", "Rocero Sánchez", "Santo Domingo", new string[] { "0997558111", "0987425345", "0998756312" });
-System.Console.WriteLine(estudiante.getEstudiante());
-System.Console.WriteLine(estudiante.getTelefonos());
+// //Ejecuta semana3
+// Estudiante estudiante = new Estudiante("a1", "María Elena", "Rocero Sánchez", "Santo Domingo", new string[] { "0997558111", "0987425345", "0998756312" });
+// System.Console.WriteLine(estudiante.getEstudiante());
+// System.Console.WriteLine(estudiante.getTelefonos());
 
-string[] telefonos = estudiante.getTelefonos();
-System.Console.WriteLine("El teléfono 1 es: " + telefonos[0]);
-System.Console.WriteLine("El teléfono 2 es: " + telefonos[1]);
-System.Console.WriteLine("El teléfono 3 es: " + telefonos[2]);
+// string[] telefonos = estudiante.getTelefonos();
+// System.Console.WriteLine("El teléfono 1 es: " + telefonos[0]);
+// System.Console.WriteLine("El teléfono 2 es: " + telefonos[1]);
+// System.Console.WriteLine("El teléfono 3 es: " + telefonos[2]);
