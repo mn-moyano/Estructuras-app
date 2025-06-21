8f848e1cf8f22100ad1d6b126e19171f00d3254d
mn-moyano <mn.moyanoz@uea.edu.ec>
2025-06-20 23:23:38 -0500

    agregando comentarios

--- PRACTICO EXPERIMENTAL/Program.cs
+++ PRACTICO EXPERIMENTAL/Program.cs
@@ -1,18 +1,35 @@
 ﻿//Crear una Agenda de turnos de pacientes de una clínica
 //Crear las clases paciente y turno
 
-public class Paciente
+using System.Collections.Concurrent;
+using System.Net.WebSockets;
+
+class Paciente
 {
     public string Cedula;
     public string Nombre;
     public int Edad;
 
+    //Método para mostrar los datos del paciente
+    public void MostrarDatos()
+    {
+        System.Console.WriteLine($"Cédula: {Cedula}, Nombre: {Nombre}, Edad: {Edad}");
+    }
+}
 
-public Paciente(string cedula, string nombre, int edad)
+class turno
 {
-    Cedula = cedula;
-    Nombre = nombre;
-    Edad = edad;
-}
+    public Paciente paciente; //Quiere decir que un turno contiene a un paciente
+    public string FechaHora; //Fecha y hora del turno
+    public void MostrarTurno()
+    {
+        System.Console.WriteLine("====Turno====");
+        paciente.MostrarDatos();
+        System.Console.WriteLine($"Fecha y hora del turno: {FechaHora}");
+    }
 }
 
+class Agenda
+{
+    
+}
