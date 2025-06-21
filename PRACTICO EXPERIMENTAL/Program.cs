a8b039ff3837d06ecd20785d2e63be9955acb3db
mn-moyano <mn.moyanoz@uea.edu.ec>
2025-06-20 21:39:51 -0500

    avances CPE

---
+++ PRACTICO EXPERIMENTAL/Program.cs
@@ -0,0 +1,18 @@
+﻿//Crear una Agenda de turnos de pacientes de una clínica
+//Crear las clases paciente y turno
+
+public class Paciente
+{
+    public string Cedula;
+    public string Nombre;
+    public int Edad;
+
+
+public Paciente(string cedula, string nombre, int edad)
+{
+    Cedula = cedula;
+    Nombre = nombre;
+    Edad = edad;
+}
+}
+
