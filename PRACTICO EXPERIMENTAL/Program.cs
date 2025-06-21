1e81e23b651d2bcfc7042471601af113cb936372
mn-moyano <mn.moyanoz@uea.edu.ec>
2025-06-20 23:36:59 -0500

    Creando búsqueda por cédula

--- PRACTICO EXPERIMENTAL/Program.cs
+++ PRACTICO EXPERIMENTAL/Program.cs
@@ -21,7 +21,7 @@ class turno
 {
     public Paciente paciente; //Quiere decir que un turno contiene a un paciente
     public string FechaHora; //Fecha y hora del turno
-    public void MostrarTurno()
+    public void MostrarTurno() //Muestra los datos completos del turno
     {
         System.Console.WriteLine("====Turno====");
         paciente.MostrarDatos();
@@ -31,5 +31,43 @@ class turno
 
 class Agenda
 {
-    
+    private turno[] turnos = new turno[10]; //Arreglo de 10 turnos máximo
+    private int contador = 0; //Lleva la cuenta de los turnos ingresados
+
+    //Agregar turnos si hay disponibilidad
+    public void AgregarTurno(turno nuevoTurno)
+    {
+        if (contador < turnos.Length)
+        {
+            turnos[contador] = nuevoTurno;
+            contador++;
+            System.Console.WriteLine("Turno agregado correctamente.");
+        }
+        else
+        {
+            System.Console.WriteLine("Error, no se puede agendar turno. Agenda llena.");
+        }
+    }
+
+    //Muestra los turnos agendados
+    public void MostrarTurnos()
+    {
+        System.Console.WriteLine("\n Lista de Turnos:");
+        if (contador == 0)
+        {
+            System.Console.WriteLine("No hay turnos agendados.");
+            return;
+        }
+        for (int i = 0; i < contador; i++)
+        {
+            turnos[i].MostrarTurno();
+        }
+    }
+
+    //Buscar turnos por número de cédula
+
+    public void BuscarPorCedula(string cedula)
+    {
+        System.Console.WriteLine();
+    }
 }
