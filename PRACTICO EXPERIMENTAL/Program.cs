b44c923f7534871bbfd1566df10cc44c64894e8e
mn-moyano <mn.moyanoz@uea.edu.ec>
2025-06-24 18:07:34 -0500

    Crear pacientes y turnos

--- PRACTICO EXPERIMENTAL/Program.cs
+++ PRACTICO EXPERIMENTAL/Program.cs
@@ -1,3 +1,9 @@
 ﻿//Programa que da marcha a la agenda de la clínica
+System.Console.WriteLine("Agenda de Turnos de Pacientes");
+System.Console.WriteLine("=============================");
 
+int max = 10;
+Turno[] turnos = new Turno[max];
+
+//Crear pacientes y turnos
 
