959cdfd5d87f7ec02127bc7a5309f2b01f3f0535
mn-moyano <mn.moyanoz@uea.edu.ec>
2025-06-24 18:10:58 -0500

    pacientes

--- PRACTICO EXPERIMENTAL/Program.cs
+++ PRACTICO EXPERIMENTAL/Program.cs
@@ -6,4 +6,7 @@ int max = 10;
 Turno[] turnos = new Turno[max];
 
 //Crear pacientes y turnos
+Paciente paciente1 = new Paciente("1708402449", "Rodrigo", 58);
+Paciente paciente2 = new Paciente("1201524856", "Rosa", 25);
+Paciente paciente3 = new Paciente("2300048952", "Madelyn", 28);
 
