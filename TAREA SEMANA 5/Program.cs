13358e25db65dbd41f50abb8f81b0572d9a08f6a
mn-moyano <mn.moyanoz@uea.edu.ec>
2025-06-29 00:26:59 -0500

    Lista de asiganaturas

--- TAREA SEMANA 5/Program.cs
+++ TAREA SEMANA 5/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("UNIVERSIDAD ESTATAL AMAZÓNICA");
+System.Console.WriteLine("=============================");
+
+//Crear una lista de asiganturas
+List<string> asignaturas = new List<string> { "Matemáticas", "Física", "Química", "Historia", "Lengua" };
+
+//Imprimir la lista
+foreach (var asigantura in asignaturas)
+{
+     System.Console.WriteLine("Yo estudio " + asigantura);
+}
