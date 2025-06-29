2460fe091b27efc29e0e96f08005158f8336b13d
mn-moyano <mn.moyanoz@uea.edu.ec>
2025-06-29 00:50:21 -0500

    Preguntar notas

--- TAREA SEMANA 5/Program.cs
+++ TAREA SEMANA 5/Program.cs
@@ -4,8 +4,25 @@ System.Console.WriteLine("=============================");
 //Crear una lista de asiganturas
 List<string> asignaturas = new List<string> { "Matemáticas", "Física", "Química", "Historia", "Lengua" };
 
+//Crear lista de notas
+List<double> notas = new List<double>();
+
 //Imprimir la lista
-foreach (var asigantura in asignaturas)
+ foreach (var asigantura in asignaturas)
+ {
+    System.Console.WriteLine("Yo estudio " + asigantura);
+ }
+
+//Ingrese nota
+for (int i = 0; i < asignaturas.Count; i++)
 {
-     System.Console.WriteLine("Yo estudio " + asigantura);
+    System.Console.WriteLine("¿Qué nota has sacado en " + asignaturas[i] + "?");
+    double nota = Convert.ToDouble(Console.ReadLine());
+    notas.Add(nota);
+}
+
+System.Console.WriteLine("====Resultados====");
+for (int i = 0; i < asignaturas.Count; i++) 
+{
+    System.Console.WriteLine("En " + asignaturas[i] + " has sacado " + notas[i]);
 }
