1561cde43aa6ef50e00327b50a3cd8f1bc58f92d
mn-moyano <mn.moyanoz@uea.edu.ec>
2025-06-29 01:30:09 -0500

    Orden inverso de números

--- TAREA SEMANA 5/Program.cs
+++ TAREA SEMANA 5/Program.cs
@@ -51,3 +51,24 @@ foreach (var numero in numerosGanadores)
     Console.WriteLine(numero);
 }
 
+//Lista de números del 1 al 10
+List<int> numeros = new List<int>();
+
+for (int i = 1; i <= 10; i++)
+{
+    numeros.Add(i);
+}
+
+//Invertir el orden de la lista
+numeros.Reverse();
+
+//Mostrar los números en inverso y separados por comas
+System.Console.WriteLine();
+System.Console.WriteLine("========Números en orden inverso del 1 al 10======");
+for (int i = 0; i < numeros.Count; i++)
+{
+    if (i < numeros.Count - 1)
+        Console.Write(numeros[i] + ", ");
+    else
+        Console.Write(numeros[i]);
+}
