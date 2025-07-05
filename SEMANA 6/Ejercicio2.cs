f65999bc9d3c932244a267c1522363b7debd3e0f
mn-moyano <mn.moyanoz@uea.edu.ec>
2025-07-05 16:40:35 -0500

    Probar el funcionamiento

---
+++ SEMANA 6/Ejercicio2.cs
@@ -0,0 +1,79 @@
+//Implementar el método buscar en la clase lista y retornar el número de veces que se encuentra el dato dentro de la lista.
+//Crear la clase Nodo
+public class Nodo2
+{
+    public int Dato;
+    public Nodo2 Siguiente;
+
+    public Nodo2(int _dato)
+    {
+        Dato = _dato;
+        Siguiente = null;
+    }
+}
+
+//Crear la clase lista enlazada
+public class ListaEnlazada2
+{
+    public Nodo2 Cabeza;
+    public void AgregarAlFinal(int _dato)
+    {
+        Nodo2 nuevo = new Nodo2(_dato);
+        if (Cabeza == null)
+        {
+            Cabeza = nuevo;
+        }
+        else
+        {
+            Nodo2 actual = Cabeza;
+            while (actual.Siguiente != null)
+            {
+                actual = actual.Siguiente;
+            }
+            actual.Siguiente = nuevo;
+        }
+    }
+
+    //Mostrar lista original
+    public void Mostrar()
+    {
+        Nodo2 actual = Cabeza;
+        while (actual != null)
+        {
+            System.Console.Write(actual.Dato + " -> ");
+            actual = actual.Siguiente;
+        }
+        System.Console.WriteLine("null");
+    }
+
+//Método para buscar datos en la lista
+    public ListaEnlazada2 BuscarDatoLista(int valorBuscado)
+    {
+        Nodo2 actual = Cabeza;
+        ListaEnlazada2 encontrados = new ListaEnlazada2();
+        int contador = 0;
+
+        while (actual != null)
+        {
+            if (actual.Dato == valorBuscado)
+            {
+                encontrados.AgregarAlFinal(actual.Dato);
+                contador++;
+            }
+            actual = actual.Siguiente;
+        }
+
+        if (contador == 0)
+        {
+            System.Console.WriteLine($"El valor {valorBuscado} no fue encontrado en la lista.");
+        }
+        else
+        {
+            System.Console.WriteLine($"El valor {valorBuscado} fue encontrado {contador} veces.");
+            System.Console.WriteLine("Elementos encontrados: ");
+            encontrados.Mostrar();
+        }
+
+        return encontrados;
+    }
+}
--- SEMANA 6/Program.cs
+++ SEMANA 6/Program.cs
@@ -27,3 +27,21 @@ lista.EliminarRango(minimo, maximo);
 //Mostrar la nueva lista después de eliminar nodos
 System.Console.WriteLine("Lista 2: Luego de eliminar los Nodos fuera de rango ");
 lista.Mostrar(); 
+
+System.Console.WriteLine();
+//Crear el programa que ejecuta las acciones del ejercicio2
+ListaEnlazada2 lista2 = new ListaEnlazada2();
+Random rnd2 = new Random();
+
+for (int i = 0; i < 50; i++)
+{
+    lista2.AgregarAlFinal(rnd2.Next(1, 1000));
+}
+
+System.Console.WriteLine("Lista 2 original: ");
+lista2.Mostrar();
+
+System.Console.WriteLine("Ingrese un valor para buscar: ");
+int valor = int.Parse(Console.ReadLine());
+
+ListaEnlazada2 encontrados = lista2.BuscarDatoLista(valor);
