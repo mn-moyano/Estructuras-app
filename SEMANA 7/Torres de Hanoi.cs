7d0a52939f44b0b4b66bdbc5ac4955da01c02ae3
mn-moyano <mn.moyanoz@uea.edu.ec>
2025-07-12 13:31:12 -0500

    Pedimos al usuario ingresar el número de discos

--- SEMANA 7/Torres de Hanoi.cs
+++ SEMANA 7/Torres de Hanoi.cs
@@ -1,8 +1,16 @@
 //Crear pila
+using System.Runtime.InteropServices;
+
 public class TorresDeHanoi
 {
     public static void run()
     {
-        
+        System.Console.WriteLine("Ingrese el número de discos a mover: ");
+        int n;
+
+        while (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+        {
+            
+        }
     }
 }
