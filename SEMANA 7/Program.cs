82d88e1c0982429c71ef4f27186ddbe70eff5486
mn-moyano <mn.moyanoz@uea.edu.ec>
2025-07-12 13:07:38 -0500

    Colocamos el programa que ejecuta la clase paréntesis

--- SEMANA 7/Program.cs
+++ SEMANA 7/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.InteropServices;
+
+Console.WriteLine("UNIVERSIDAD ESTATAL AMAZÓNICA");
+Console.WriteLine("=============================");
+System.Console.WriteLine();
+
+Parentesis.run();
