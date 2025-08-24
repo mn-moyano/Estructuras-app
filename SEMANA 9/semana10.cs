8a1f04c3b063cca1ff36b32f658037eeb2db1a0c
mn-moyano <mn.moyanoz@uea.edu.ec>
2025-08-24 16:19:25 -0500

    Crear y verificar el funcionamiento del código

--- SEMANA 9/semana10.cs
+++ SEMANA 9/semana10.cs
@@ -4,17 +4,25 @@ public class vacunas
     {
         Random random = new Random();
 
+        HashSet<string> ciudadanos = new HashSet<string>();
+        for (int i = 0; i <= 500; i++)
+        {
+            ciudadanos.Add("Ciudadano " + i);
+        }
+
         HashSet<string> pfizer = new HashSet<string>();
         for (int i = 0; i < 75; i++)
-        {
-            pfizer.Add("persona " + random.Next(1, 500));
-        }
+            while (pfizer.Count < 75)
+            {
+                pfizer.Add("Ciudadano " + random.Next(1, 500));
+            }
 
         HashSet<string> astrazeneca = new HashSet<string>();
         for (int i = 0; i < 75; i++)
-        {
-            astrazeneca.Add("persona " + random.Next(1, 500));
-        }
+            while (astrazeneca.Count < 75)
+            {
+                astrazeneca.Add("Ciudadano " + random.Next(1, 500));
+            }
         foreach (var item in pfizer)
         {
             System.Console.WriteLine(item);
@@ -25,14 +33,31 @@ public class vacunas
         {
             System.Console.WriteLine(item);
         }
-        System.Console.WriteLine("Vacunados solo con pfizer: " + soloPfizer.Count);
-    }
+        System.Console.WriteLine("Ciudadanos vacunados solo con pfizer: " + soloPfizer.Count);
 
+        HashSet<string> soloAstrazeneca = new HashSet<string>();
+        soloAstrazeneca.UnionWith(astrazeneca);
+        foreach (var item in soloAstrazeneca)
+        {
+            System.Console.WriteLine(item);
+        }
+        System.Console.WriteLine("Ciudadanos vacunados solo con Astrazeneca: " + soloAstrazeneca.Count);
 
+        HashSet<string> ambasDosis = new HashSet<string>(pfizer.Intersect(astrazeneca));
+        foreach (var item in ambasDosis)
+        {
+            System.Console.WriteLine(item);
+        }
+        System.Console.WriteLine("Ciudadanos vacunados con ambas dosis: " + ambasDosis.Count);
 
-    //private static HashSet<string>() (string vacuna, int cantidad){
+        HashSet<string> vacunados = new HashSet<string>(pfizer.Union(astrazeneca));
 
-    //}
-    
+        HashSet<string> noVacunados = new HashSet<string>(ciudadanos.Except(vacunados));
+        foreach (var item in noVacunados)
+        {
+            System.Console.WriteLine(item);
+        }
+        System.Console.WriteLine("Ciudadanos no vacunados: " + noVacunados.Count);
+    }  
 
 }
