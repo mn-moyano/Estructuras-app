deb44c6ae71492f191965e15ed2826893d47e0b9
mn-moyano <mn.moyanoz@uea.edu.ec>
2025-08-24 16:25:03 -0500

    Comentar el código

--- SEMANA 9/semana10.cs
+++ SEMANA 9/semana10.cs
@@ -1,23 +1,23 @@
-public class vacunas
+public class vacunas //Crear la clase vacunas
 {
-    public static void run()
+    public static void run() //Crear un método
     {
-        Random random = new Random();
+        Random random = new Random(); //Utilizar datos randómicos
 
-        HashSet<string> ciudadanos = new HashSet<string>();
+        HashSet<string> ciudadanos = new HashSet<string>(); //Crear un conjunto de 500 ciudadanos
         for (int i = 0; i <= 500; i++)
         {
             ciudadanos.Add("Ciudadano " + i);
         }
 
-        HashSet<string> pfizer = new HashSet<string>();
+        HashSet<string> pfizer = new HashSet<string>(); //Determinar 75 personas que tengan la vacuna Pfizer
         for (int i = 0; i < 75; i++)
             while (pfizer.Count < 75)
             {
                 pfizer.Add("Ciudadano " + random.Next(1, 500));
             }
 
-        HashSet<string> astrazeneca = new HashSet<string>();
+        HashSet<string> astrazeneca = new HashSet<string>(); //Determinar 75 personas que tienen la vacuna AstraZeneca
         for (int i = 0; i < 75; i++)
             while (astrazeneca.Count < 75)
             {
@@ -27,7 +27,8 @@ public class vacunas
         {
             System.Console.WriteLine(item);
         }
-        HashSet<string> soloPfizer = new HashSet<string>();
+
+        HashSet<string> soloPfizer = new HashSet<string>(); //Mostrar los ciudadanos que solo están vacunados con Pfizer
         soloPfizer.UnionWith(pfizer);
         foreach (var item in soloPfizer)
         {
@@ -35,7 +36,7 @@ public class vacunas
         }
         System.Console.WriteLine("Ciudadanos vacunados solo con pfizer: " + soloPfizer.Count);
 
-        HashSet<string> soloAstrazeneca = new HashSet<string>();
+        HashSet<string> soloAstrazeneca = new HashSet<string>(); //Mostrar los ciudadanos que solo están vacunados con AstraZeneca
         soloAstrazeneca.UnionWith(astrazeneca);
         foreach (var item in soloAstrazeneca)
         {
@@ -43,16 +44,16 @@ public class vacunas
         }
         System.Console.WriteLine("Ciudadanos vacunados solo con Astrazeneca: " + soloAstrazeneca.Count);
 
-        HashSet<string> ambasDosis = new HashSet<string>(pfizer.Intersect(astrazeneca));
+        HashSet<string> ambasDosis = new HashSet<string>(pfizer.Intersect(astrazeneca)); //Mostrar los ciudadanos que están vacunados con ambas dosis
         foreach (var item in ambasDosis)
         {
             System.Console.WriteLine(item);
         }
         System.Console.WriteLine("Ciudadanos vacunados con ambas dosis: " + ambasDosis.Count);
 
-        HashSet<string> vacunados = new HashSet<string>(pfizer.Union(astrazeneca));
+        HashSet<string> vacunados = new HashSet<string>(pfizer.Union(astrazeneca)); //Crear un conjunto de ciudadanos vacunados para poder restar los no vacunados
 
-        HashSet<string> noVacunados = new HashSet<string>(ciudadanos.Except(vacunados));
+        HashSet<string> noVacunados = new HashSet<string>(ciudadanos.Except(vacunados)); //Mostrar los ciudadanos no vacunados
         foreach (var item in noVacunados)
         {
             System.Console.WriteLine(item);
