046426712f865ad3730d558f07a9c183a863336e
mn-moyano <mn.moyanoz@uea.edu.ec>
2025-08-30 07:56:50 -0500

    pruebas para generar reporte pdf

---
+++ PdfReportApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using PdfSharpCore.Drawing;
+using PdfSharpCore.Pdf;
+
+class Program
+{
+    static void Main()
+    {
+        var document = new PdfDocument();
+        document.Info.Title = "Reporte PDF";
+
+        var page = document.AddPage();
+        var gfx = XGraphics.FromPdfPage(page);
+        var font = new XFont("Verdana", 20, XFontStyle.Bold);
+
+        gfx.DrawString("¡Hola, Madelyn!", font, XBrushes.Black,
+            new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
+
+        document.Save("ReporteMadelyn.pdf");
+    }
+}
