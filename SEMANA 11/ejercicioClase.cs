51fc032a09ffbb1d7042668b9c50eba1339ddf44
mn-moyano <mn.moyanoz@uea.edu.ec>
2025-08-30 08:07:17 -0500

    Crear nuevo proyecto para la semana 11

--- SEMANA 11/ejercicioClase.cs
+++ SEMANA 11/ejercicioClase.cs
@@ -2,10 +2,17 @@ public class ClaveValor
 {
     public static void run()
     {
+        //Definición de un diccionario
         Dictionary<int, string> estudiantes = new Dictionary<int, string>();
         estudiantes.Add(1, "Juan");
         estudiantes.Add(2, "Pedro");
         estudiantes.Add(3, "Maria");
         estudiantes.Add(4, "Anabel");
+
+        //Definición e inicialización de un diccionario
+        Dictionary<int, string> docentes = new()
+        {
+            {1, "Anita"}
+        };
     }
 }
