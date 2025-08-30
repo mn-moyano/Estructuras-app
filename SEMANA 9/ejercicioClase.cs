aba46d0d33758d3d496157b54e35829c2800d7c5
mn-moyano <mn.moyanoz@uea.edu.ec>
2025-08-30 08:01:50 -0500

    Definición de diccionario

---
+++ SEMANA 9/ejercicioClase.cs
@@ -0,0 +1,11 @@
+public class ClaveValor
+{
+    public static void run()
+    {
+        Dictionary<int, string> estudiantes = new Dictionary<int, string>();
+        estudiantes.Add(1, "Juan");
+        estudiantes.Add(2, "Pedro");
+        estudiantes.Add(3, "Maria");
+        estudiantes.Add(4, "Anabel");
+    }
+}
