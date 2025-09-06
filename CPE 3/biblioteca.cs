a56a5dc71c724a4c4017d1860c8e3e3b077c9adc
mn-moyano <mn.moyanoz@uea.edu.ec>
2025-09-05 20:54:14 -0500

    Hacer pruebas y verificar funcionamiento

--- CPE 3/biblioteca.cs
+++ CPE 3/biblioteca.cs
@@ -13,19 +13,19 @@ public class Biblioteca //Crear la clase biblioteca
         libros.Add(2, "Miguel de Cervantes - Don Quijote de la Mancha");
         libros.Add(3, "Julio Verne - Viaje al Centro de la Tierra");
 
-
-        while (true)
+        int opcion = -1;
+        while (opcion != 0)
         {
             System.Console.WriteLine("=======Menú=====");
             System.Console.WriteLine("1. Buscar libro por autor(id)");
             System.Console.WriteLine("2. Agregar libro");
             System.Console.WriteLine("3. Mostrar libros");
-            System.Console.WriteLine("4. Salir");
+            System.Console.WriteLine("0. Salir");
             System.Console.WriteLine("Elige una opción: ");
+            opcion = int.Parse(Console.ReadLine());
+            
 
-            string opcion = Console.ReadLine();
-
-            if (opcion == "1")
+            if (opcion == 1)
             {
                 System.Console.WriteLine("Ingrese el ID del libro a buscar: ");
                 int id = int.Parse(Console.ReadLine());
@@ -39,14 +39,43 @@ public class Biblioteca //Crear la clase biblioteca
                     System.Console.WriteLine("No se encontró libro con ese ID.");
                 }
             }
-            else if (opcion == "2")
+            else if (opcion == 2)
             {
                 System.Console.WriteLine("Ingrese ID del libro: ");
                 int id = int.Parse(Console.ReadLine());
 
                 System.Console.WriteLine("Ingrese título del libro: ");
                 string titulo = Console.ReadLine();
-                
+
+                System.Console.WriteLine("Ingrese autor del libro: ");
+                string autor = Console.ReadLine();
+
+                if (titulos.Contains(titulo))
+                {
+                    System.Console.WriteLine($"El libro '{titulo}' ya está registrado.");
+                }
+                else
+                {
+                    titulos.Add(titulo);
+                    libros[id] = $"{autor} - {titulo}";
+                    System.Console.WriteLine("Libro agregado correctamente.");
+                }
+            }
+            else if (opcion == 3)
+            {
+                System.Console.WriteLine("Listado de libros registrados:");
+                foreach (var item in libros)
+                {
+                    System.Console.WriteLine($"ID: {item.Key}, {item.Value}");
+                }
+            }
+            else if (opcion == 0)
+            {
+                System.Console.WriteLine("Saliendo...");
+            }
+            else
+            {
+                System.Console.WriteLine("Error, opción inválida.");
             }
         }
     }
